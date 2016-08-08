284647b4d0e3a298e57a51b1b3dcb045d62c4eb2
Jaemoon <jaemoon@jaemoonpark.com>
2016-08-08 15:55:42 -0400

    FIX: fix bug where textfield appears behind image after selecting image ADD: add feature to save meme image and to share it

--- MemeMe 1.0/ViewController.swift
+++ MemeMe 1.0/ViewController.swift
@@ -12,10 +12,14 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     @IBOutlet weak var btnCamera: UIBarButtonItem!
     @IBOutlet weak var btnAlbum: UIBarButtonItem!
+    @IBOutlet weak var btnShare: UIBarButtonItem!
+    @IBOutlet weak var txtFieldTop: UITextField!
+    @IBOutlet weak var txtFieldBtm: UITextField!
     @IBOutlet weak var viewImage: UIImageView!
 
     override func viewDidLoad() {
         btnCamera.enabled = UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)
+        btnShare.enabled = false
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -38,10 +42,45 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         controllerAlbum.delegate = self
         self.presentViewController(controllerAlbum, animated: true, completion: nil)
     }
+    
+    @IBAction func shareMeme(){
+        //storing original bounds
+        let boundOrigImage = viewImage.bounds
+        let boundOrigTopTxt = txtFieldTop.bounds
+        let boundOrigBtmTxt = txtFieldBtm.bounds
+        
+        //initializing and setting up bounds for meme capture
+        viewImage.bounds = CGRect(x: 0.0, y: 0.0, width: viewImage.image!.size.width, height: viewImage.image!.size.height)
+        txtFieldTop.bounds = CGRect(x: 0.0, y: 0.0, width: viewImage.image!.size.width, height: txtFieldTop.bounds.height)
+        txtFieldBtm.bounds = CGRect(x: 0.0, y: viewImage.image!.size.height - txtFieldBtm.bounds.height, width: viewImage.image!.size.width, height: txtFieldBtm.bounds.height)
+        
+        //creating memed image
+        UIGraphicsBeginImageContextWithOptions(viewImage.bounds.size, false, 1.0)
+        viewImage.drawViewHierarchyInRect(viewImage.bounds, afterScreenUpdates: true)
+        txtFieldTop.drawViewHierarchyInRect(txtFieldTop.bounds, afterScreenUpdates: true)
+        txtFieldBtm.drawViewHierarchyInRect(txtFieldBtm.bounds, afterScreenUpdates: true)
+        
+        //creating and saving mememed image to variable
+        let memedImage: UIImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        //restoring original bounds
+        viewImage.bounds = boundOrigImage
+        txtFieldTop.bounds = boundOrigTopTxt
+        txtFieldBtm.bounds = boundOrigBtmTxt
+        
+        //presenting activity view controller
+        let activityView = UIActivityViewController(activityItems: [memedImage], applicationActivities: nil)
+        self.presentViewController(activityView, animated: true, completion: nil)
+        
+    }
 
     func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
         viewImage.image = image
         dismissViewControllerAnimated(true, completion: nil)
+        btnShare.enabled = true
+        //makes sure textfields are visually accessible
+        self.view.sendSubviewToBack(viewImage)
     }
 
 }
