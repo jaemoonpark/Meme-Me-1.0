bed6e8b45c147f447d839c5f096f4221e2e13b5a
Jaemoon <jaemoon@jaemoonpark.com>
2016-08-08 15:08:55 -0400

    create and implement camera and album button functions

--- MemeMe 1.0/ViewController.swift
+++ MemeMe 1.0/ViewController.swift
@@ -8,9 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var btnCamera: UIBarButtonItem!
+    @IBOutlet weak var btnAlbum: UIBarButtonItem!
+    @IBOutlet weak var viewImage: UIImageView!
 
     override func viewDidLoad() {
+        btnCamera.enabled = UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -19,7 +24,25 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func pickCamera(){
+        let controllerCamera = UIImagePickerController()
+        controllerCamera.sourceType = UIImagePickerControllerSourceType.Camera
+        controllerCamera.delegate = self
+        self.presentViewController(controllerCamera, animated: true, completion: nil)
+    }
+    
+    @IBAction func pickAlbum(){
+        let controllerAlbum = UIImagePickerController()
+        controllerAlbum.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        controllerAlbum.delegate = self
+        self.presentViewController(controllerAlbum, animated: true, completion: nil)
+    }
 
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
+        viewImage.image = image
+        dismissViewControllerAnimated(true, completion: nil)
+    }
 
 }
 
